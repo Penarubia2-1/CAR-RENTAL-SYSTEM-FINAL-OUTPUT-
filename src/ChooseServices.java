82080ebc2b821150a12c89ee7f62e1baa16fda92
639319165346 <639319165346@jurieimperial>
2024-06-25 13:08:55 +0800

    Imperial

--- src/ChooseServices.java
+++ src/ChooseServices.java
@@ -5,9 +5,12 @@
 @author Imperial Jurie
  */
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -17,7 +20,7 @@ import javax.swing.JPanel;
  * @author jurie imperial
  */
 public class ChooseServices  extends JFrame implements ActionListener{
-    private JLabel lblCarRentalSystem;
+    private JLabel lblCarRentalSystem,img;
     private JButton btnAvailableVehicle,btnReservation,btnOtherservices,btnback;
    
     
@@ -26,10 +29,16 @@ public class ChooseServices  extends JFrame implements ActionListener{
         setSize(600,500);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-     lblCarRentalSystem = new JLabel("CAR RENTAL SYSTEM");
+        getContentPane().setBackground(Color.black);
+        
+        img = new JLabel(); 
+        img.setIcon(new ImageIcon(new ImageIcon("C:\\Users\\63931\\OneDrive\\Documents\\NetBeansProjects\\CAR-RENTAL-SYSTEM-FINAL-OUTPUT-1\\src\\imgservices.png").getImage().getScaledInstance(500, 400, Image.SCALE_SMOOTH)));
+        img.setBounds(10,100, 500, 500);
+        
+        lblCarRentalSystem = new JLabel("CAR RENTAL SYSTEM");
      lblCarRentalSystem.setBounds(150,50,300,30);
      lblCarRentalSystem.setFont(new Font("Arial", Font.BOLD, 25));
+     lblCarRentalSystem.setForeground(Color.white);
      
      
      btnAvailableVehicle = new JButton("AVAILABLE VEHICLE");
@@ -45,7 +54,7 @@ public class ChooseServices  extends JFrame implements ActionListener{
      btnOtherservices.setFont(new Font("Arial", Font.BOLD,15));
      
      btnback = new JButton("BACK");
-     btnback.setBounds(210,300,150, 30);
+     btnback.setBounds(210,280,150, 30);
      btnback.setFont(new Font("Arial", Font.BOLD,15));
      
      
@@ -60,7 +69,7 @@ public class ChooseServices  extends JFrame implements ActionListener{
      add(btnAvailableVehicle);
      add(btnReservation);
      add(btnback);
-     
+     add(img);
     }
     @Override
    public void actionPerformed(ActionEvent e){
