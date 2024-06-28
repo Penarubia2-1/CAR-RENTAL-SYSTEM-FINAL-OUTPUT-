873603068cb3a3e388da864a72030ad62fdc37f1
John Mark Penarubia <lenovo@LAPTOP-HLI9NLRT>
2024-06-28 09:23:28 +0800

    ADMIN BUTTONS

--- src/Admin.java
+++ src/Admin.java
@@ -12,27 +12,41 @@ import javax.swing.*;
 import java.awt.event.*;
 public class Admin extends JFrame implements ActionListener {
     JLabel lblHiAdmin;
-    JButton btnexit;
+    JButton btnexit,btnAdd,btnDelete,btnUpdate;
     Admin(){
         setTitle("Hi Admin!");
         setSize(1000,700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         
-        lblHiAdmin=new JLabel("You Are Admin!");
+        lblHiAdmin = new JLabel("Welcome, Admin!",SwingConstants.CENTER);
         lblHiAdmin.setBounds(400, 30, 150, 80);
-        lblHiAdmin.setFont(new Font("Arial", Font.BOLD,15));
+        lblHiAdmin.setFont(new Font("Arial", Font.BOLD, 15));
 
         
-        btnexit=new JButton("EXIT");
-        btnexit.setBounds(500,500,80,60);
+        btnexit=new JButton("LOG OUT");
+        btnexit.setBounds(700,500,120,40);
+       
+        btnAdd = new JButton("ADD");
+        btnAdd.setBounds(200, 500, 80, 40);
+
+        btnDelete = new JButton("DELETE");
+        btnDelete.setBounds(350, 500, 100, 40);
+
+        btnUpdate = new JButton("UPDATE");
+        btnUpdate.setBounds(500, 500, 100, 40);
         
         //add
         add(lblHiAdmin);
         add(btnexit);
-        
+        add(btnAdd);
+        add(btnDelete);
+        add(btnUpdate);
         //add
         btnexit.addActionListener(this);
+        btnAdd.addActionListener(this);
+        btnDelete.addActionListener(this);
+        btnUpdate.addActionListener(this);
     }
 
     @Override
