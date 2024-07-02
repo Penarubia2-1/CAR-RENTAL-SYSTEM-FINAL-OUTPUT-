606f15301780d4f697f45bcd3c85c66a481a5a4e
John Mark Penarubia <lenovo@LAPTOP-HLI9NLRT>
2024-07-02 12:36:54 +0800

    update

--- src/GenerateInvoicesMain.java
+++ src/GenerateInvoicesMain.java
@@ -15,6 +15,7 @@ public class GenerateInvoicesMain {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
        EventQueue.invokeLater(new Runnable() {
